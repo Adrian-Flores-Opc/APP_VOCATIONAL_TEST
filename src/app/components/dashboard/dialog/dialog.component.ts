5f7ee590bd9e3e4c4dcb75b12550e5e99265cf20
T09351 <T09351@W204SISF35119S>
2022-11-21 18:43:15 -0400

    adecuaciones muestreo universidades

--- src/app/components/dashboard/dialog/dialog.component.ts
+++ src/app/components/dashboard/dialog/dialog.component.ts
@@ -1,5 +1,8 @@
+import { UniversitiesResponse } from './../../../model/universities/universities.model';
+import { CareersResponse } from 'src/app/model/careers/careers.model';
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ConeectionApiService } from 'src/app/core/service-connection/coneection-api.service';
 
 @Component({
   selector: 'app-dialog',
@@ -8,11 +11,33 @@ import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dial
 })
 export class DialogComponent implements OnInit {
 
-  constructor(private dialogRef: MatDialogRef<DialogComponent>, @Inject(MAT_DIALOG_DATA) public data: any ) { 
-    console.log('DATO RECIVIDO: ' + JSON.stringify(data));
-  }
+  public _carrera !: String;
+  public _universitiesResponse : UniversitiesResponse[] = [];
+  constructor(private dialogRef: MatDialogRef<DialogComponent>, @Inject(MAT_DIALOG_DATA) public data: any, private _serviceConnection: ConeectionApiService ) { }
 
   ngOnInit(): void {
+    this.getUniversitiesByCarrera();
   }
 
+  public getUniversitiesByCarrera():void{
+    this._serviceConnection.getCareers().subscribe({ next: (_response) => {
+      let _carrerasSearch : CareersResponse[] = [];
+      console.log('CARRERA A FILTRAR: ' + this.data.carrera);
+      _carrerasSearch = _response.filter(x => x.CAREERS === this.data.carrera);
+      _carrerasSearch.forEach( element => {
+        this._serviceConnection.getUniversitiesById(element.ID_UNIVERSITIES).subscribe({ next: (_response) => {
+          this._universitiesResponse.push(_response);
+        }, error: (_error) => {
+
+        }, complete:() => {
+
+        }});
+      });
+      console.log('LISTADO DE UNIVERSIDADES POR CARRERA:' + JSON.stringify(this._universitiesResponse));
+    }, error: (_error) => {
+
+    }, complete:() => {
+
+    }});
+  }
 }

515e353779bafe538397ee43b672982a4ca87f79
T09351 <T09351@W204SISF35119S>
2022-11-24 17:26:31 -0400

    adecuaciones formulario

--- src/app/components/user-register/user-register.component.ts
+++ src/app/components/user-register/user-register.component.ts
@@ -5,7 +5,8 @@ import { ServiceNotificationsService } from 'src/app/core/service-notifications/
 import { ServiceMainService } from 'src/app/core/service/service-main.service';
 import { StorageService } from 'src/app/core/session/storage.service';
 import { RegisterUser, RegisterUserRequest } from 'src/app/model/register/register-user.model';
-import Swal from 'sweetalert2';
+import { formatDate } from '@angular/common';
+
 
 @Component({
   selector: 'app-user-register',
@@ -30,7 +31,7 @@ export class UserRegisterComponent implements OnInit {
     this._requestUserRegister = new RegisterUserRequest;
     this.registerForm = new UntypedFormGroup({
       fullnameRegister: new UntypedFormControl({ value: '', disabled: false }, [Validators.required, Validators.minLength(4)] ),
-      emailRegister: new UntypedFormControl({ value: '', disabled: false }, [Validators.required, Validators.minLength(4)] ),
+      emailRegister: new UntypedFormControl({ value: '', disabled: false }, [Validators.required, Validators.minLength(4), Validators.email] ),
       cellphoneRegister: new UntypedFormControl({ value: '', disabled: false }, [Validators.required, Validators.minLength(4)] ),
       dateOfBirdRegister: new UntypedFormControl({ value: '', disabled: false }, [Validators.required, Validators.minLength(4)] ),
       nameUserRegister: new UntypedFormControl({ value: '', disabled: false }, [Validators.required, Validators.minLength(4)] ),
@@ -46,18 +47,21 @@ export class UserRegisterComponent implements OnInit {
     this._requestUserRegister.DateOfBird = _request.dateOfBird;
     this._requestUserRegister.NameUser = _request.nameUser;
     this._requestUserRegister.PassUser = _request.password;
-    this._requestUserRegister.RegistrationDate = '2022-11-15';
-    this._serviceConnection.saveUserRegister(this._requestUserRegister).subscribe({ next: (_response) => {
-      console.log('REGISTRO USUARIO: ' + JSON.stringify(_response));
-      if (_response != undefined){
-        this._serviceNotification.notificationsSimple('Usuario Registrado Correctamente.', 'success');
-      } else {
-        this._serviceNotification.notificationsSimple('No se pudo registrar el usuario.','error');
-      }
-    }, error: (_error) => {
-      console.log('ERROR: ' + _error);
-    }, complete:() => {
-      console.log('INFORMACION OBTENIDA CORRECTAMENTE');
-    } });
+    this._requestUserRegister.RegistrationDate = formatDate(Date.now(), 'yyyy-MM-dd', 'en-US');
+    if(_request.password != _request.verifyPassword){
+      this._serviceNotification.notificationsSimple('La contraseña no es igual a la ingresada en el campo "repetir contraseña".','info');
+    } else {
+      this._serviceConnection.saveUserRegister(this._requestUserRegister).subscribe({ next: (_response) => {
+        if (_response != undefined){
+          this._serviceNotification.notificationsSimple('Usuario Registrado Correctamente.', 'success');
+        } else {
+          this._serviceNotification.notificationsSimple('No se pudo registrar el usuario.','error');
+        }
+      }, error: (_error) => {
+        console.log('ERROR: ' + _error);
+      }, complete:() => {
+        console.log('INFORMACION OBTENIDA CORRECTAMENTE');
+      } });
+    }
   }
 }
